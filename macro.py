94103eb808d21ad2080e17385defef4157acf15d
Cole Boothman <coleboothman@Coles-Air.hitronhub.home>
2018-04-09 18:16:28 -0700

    Add support for absoprtions

--- macro.py
+++ macro.py
@@ -51,7 +51,7 @@ To run:
 	once an hour or so but that's about it!
 
 """
-
+import argparse
 import pyautogui, sys
 import sys
 import random
@@ -59,10 +59,38 @@ import time
 import threading
 
 # We assume that we are taking in 4 super combat pots, in the first row
-SUPER_CMB_LOCATIONS = [{'coords':[584, 306], 'doses': 4}]
-SUPER_CMB_LOCATIONS.append({'coords': [626, 306], 'doses': 4})
-SUPER_CMB_LOCATIONS.append({'coords': [668, 306], 'doses': 4})
-SUPER_CMB_LOCATIONS.append({'coords': [710, 306], 'doses': 4})
+SUPER_CMB_POTS = [{'coords':[584, 306], 'doses': 4}]
+SUPER_CMB_POTS.append({'coords': [626, 306], 'doses': 4})
+SUPER_CMB_POTS.append({'coords': [668, 306], 'doses': 4})
+SUPER_CMB_POTS.append({'coords': [710, 306], 'doses': 4})
+
+# Start with 850 Absorb pots, since this will drink the pots automatically on init.
+ABSORB_POTS = [{'coords':[584, 378], 'doses': 3}]
+# Third Row
+ABSORB_POTS.append({'coords': [626, 378], 'doses': 4})
+ABSORB_POTS.append({'coords': [668, 378], 'doses': 4})
+ABSORB_POTS.append({'coords': [710, 378], 'doses': 4})
+# Fourth Row
+ABSORB_POTS.append({'coords':[584, 414], 'doses': 4})
+ABSORB_POTS.append({'coords': [626, 414], 'doses': 4})
+ABSORB_POTS.append({'coords': [668, 414], 'doses': 4})
+ABSORB_POTS.append({'coords': [710, 414], 'doses': 4})
+# Fifth Row
+ABSORB_POTS.append({'coords':[584, 450], 'doses': 4})
+ABSORB_POTS.append({'coords': [626, 450], 'doses': 4})
+ABSORB_POTS.append({'coords': [668, 450], 'doses': 4})
+ABSORB_POTS.append({'coords': [710, 450], 'doses': 4})
+# Sixth Row
+ABSORB_POTS.append({'coords':[584, 486], 'doses': 4})
+ABSORB_POTS.append({'coords': [626, 486], 'doses': 4})
+ABSORB_POTS.append({'coords': [668, 486], 'doses': 4})
+ABSORB_POTS.append({'coords': [710, 486], 'doses': 4})
+# Seventh Row (Last inv space is the rockcake)
+ABSORB_POTS.append({'coords':[584, 522], 'doses': 4})
+ABSORB_POTS.append({'coords': [626, 522], 'doses': 4})
+ABSORB_POTS.append({'coords': [668, 522], 'doses': 4})
+
+ARGS = None
 
 # Placeholder to remember the reset HP place.
 RESET_HP_CENTER = [721, 338]
@@ -71,9 +99,9 @@ class NmzBot(object):
   """Main Bot Thread for handling drinking potion events.
 
   Attributes:
-    timer (obj): Timer thread object
-  	is_running (bool): Boolean for checking if timer thread is running.
-  	next_call(int): Current time when started the thread
+	timer (obj): Timer thread object
+	is_running (bool): Boolean for checking if timer thread is running.
+	next_call(int): Current time when started the thread
 
   """
 
@@ -85,13 +113,19 @@ class NmzBot(object):
 
   def _run(self):
 	self.is_running = False
+	self.click_inventory_icon()
 	self.drink_super_combat()
+	self.drink_absorption()
+	# Check if we are drinking absorbs (Total AFK)
+	# self.drink_absorption()
+	
+	self.click_prayer_icon()
 	self.start()
 	
   def start(self):
 	if not self.is_running:
 	  # Drink super combat at an interval of about ~17/18 mins
-	  self.next_call += random.randint(1050, 1070) 
+	  self.next_call += 20#random.randint(1050, 1070) 
 	  self._timer = threading.Timer(self.next_call - time.time(), self._run)
 	  self._timer.daemon = True # Program will end on keyboard input
 	  self._timer.start()
@@ -115,25 +149,64 @@ class NmzBot(object):
 	pyautogui.moveTo(init_reset_hp_x, init_reset_hp_y, 0.5)
 
   def drink_super_combat(self):
-	for i in range(len(SUPER_CMB_LOCATIONS)):
-		if SUPER_CMB_LOCATIONS[i]['doses'] > 0:
-			x_center = SUPER_CMB_LOCATIONS[i]['coords'][0]
-			y_center = SUPER_CMB_LOCATIONS[i]['coords'][1]
+	for i in range(len(SUPER_CMB_POTS)):
+		if SUPER_CMB_POTS[i]['doses'] > 0:
+			x_center = SUPER_CMB_POTS[i]['coords'][0]
+			y_center = SUPER_CMB_POTS[i]['coords'][1]
 
 			x_coord = random.randint(x_center-4, x_center+4)
 			y_coord = random.randint(y_center-4, y_center+5)
 
-			SUPER_CMB_LOCATIONS[i]['doses'] -= 1
-			
-			self.click_inventory_icon()
+			SUPER_CMB_POTS[i]['doses'] -= 1
+
 			pyautogui.moveTo(x_coord, y_coord, 0.5)
 			pyautogui.click()
-			self.click_prayer_icon()
-
 			break
 
+  def drink_absorption(self):
+	  doses_left = 3
+	  
+	  for i in range(len(ABSORB_POTS)):
+	  	if doses_left == 0:
+	  		return	
+		
+	  	if 	ABSORB_POTS[i]['doses'] > 0:
+			inv_space_doses = ABSORB_POTS[i]['doses']
 
-if __name__ == "__main__":
+			x_center = ABSORB_POTS[i]['coords'][0]
+			y_center = ABSORB_POTS[i]['coords'][1]
+
+			x_coord = random.randint(x_center-4, x_center+4)
+			y_coord = random.randint(y_center-4, y_center+5)
+
+			# If we have 3 or more doses in inv space, just drink the pots.
+			if doses_left <= inv_space_doses:
+				pyautogui.moveTo(x_coord, y_coord, 0.4)
+				pyautogui.click(clicks=doses_left, interval=random.uniform(2.0, 2.2))
+
+				ABSORB_POTS[i]['doses'] -= doses_left
+				doses_left -= doses_left
+			# If there aren't enough, just drink the remaining doses and move on.	
+			elif doses_left > inv_space_doses:
+				pyautogui.moveTo(x_coord, y_coord, 0.4)
+				pyautogui.click(clicks=inv_space_doses, interval=random.uniform(0.3, 0.5)) 				
+
+				ABSORB_POTS[i]['doses'] -= inv_space_doses
+				doses_left -= inv_space_doses	
+
+def get_parser():
+	parser = argparse.ArgumentParser(
+		description='NMZ Bot for AFK Training in OSRS')
+	parser.add_argument(
+		'--with_absorbs',
+		action='store_true',
+		default=False,
+		help='Drink absoprtion potions along with super combats')
+	return parser
+
+def main(argv=sys.argv[1:]):
+	ARGS = get_parser().parse_args(argv)
+
 	print('Press Ctrl-C to quit.')
 
 	# Initial reset of HP to start program
@@ -144,7 +217,7 @@ if __name__ == "__main__":
 	pyautogui.moveTo(init_reset_hp_x, init_reset_hp_y, 0.5)
 	pyautogui.click(clicks=2, interval=init_click_interval)
 
-	# The bot class handles drinking super combat pots (And absoprtions, TODO)
+	# The bot class handles drinking super combat pots (And absoprtions, if specified)
 	bot = NmzBot()
 	bot._run()
 
@@ -166,6 +239,11 @@ if __name__ == "__main__":
 
 	except (KeyboardInterrupt, SystemExit):
 		pass
+
+
+if __name__ == "__main__":
+	main()
+	
 	
 # FOR FINDING COORDS ON SCREEN 
 #
