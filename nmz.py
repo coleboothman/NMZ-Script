b3ed32732cea5405f82d59e45f3bffe3c782fd71
Cole Boothman <coleboothman2@gmail.com>
2020-04-15 12:39:17 -0700

    remove second thread and add absorbs

--- nmz.py
+++ nmz.py
@@ -2,67 +2,64 @@
 
 Written by Cole Boothman, April 2018 (Updated April 2020)
 
-This is a bot for AFK training in NMZ for OSRS. At the moment, the bot supports:
-    * Resetting the HP gain timer (clicks the hp prayer once every 45-55 secs)
-    * Drinking Super Combats every 17-18 minutes
-
-The bot will click in a random location on the coordinates given (within 5 pixels
-of x and y center coordinates to throw off autoclicking. The bot users your cursor,
-so you are effectively unable to use your computer while botting (use your labtop,
-this isn't a full blown bot client! ;)
-
-Coordinates of each item were found by centering the cursor on each location with
-the OSBuddy Client in the upper left corner of my screen.
-
-Dependencies:
-    * pip
-    * pyautogui
-
-Todo:
-    * Add support for drinking absoprtion potions
-    * Add calibration setup for other computers(?)
-    * Add a requirements file for installing dependencies
+UPDATE APRIL 2020: This is a bot for AFK training in NMZ for OSRS.
+The bot will flick your hp prayer, drink pots and absorbs pots.
 
 To run:
-    ** It's worth noting that I run this on my Macbook Air 13 inch, while using
-    spectacle to place the client in the upper left corner precisely every time. **
+- needs to be configured for each computer. Uncomment the coordinate section
+and fill in the coordinates by using the center of each icon in OSRS that
+is needed. (quick pray icon and inv slots)
 
-    * Run OSRS using OSBuddy and set screen to fixed
-    
-    * Place the OSBuddy client in the upper left corner of your screen (the script
-    currently has all coordinates hard coded, so the values may need to be changed
-    but there is a method below for finding your mouse coordinates)
+- make sure you set the threshold for combat pots repot time, and absorbs.
+for absorbs, time the amount of time it takes for the monsters to drain 1
+dose of absorb pot and use that as a reference (add like 5-10 secs extra).
 
-    * Have the following inventory setup:
-        Five Super Combat Potions (4) in the first row of your inventory
-        Rockcake in the bottom right last inventory space
-        Rest of inventory is absorption potions
+- boot up Runelite, and make sure you snap the window and always run it in
+this place on the screen, since the coordinates will rely on this.
 
-    * Gear up and start an NMZ instance (Make sure you have prayer points or the
-    script won't work!)
+- go in nmz, rock cake to 1hp, then run the script.
 
-    * Rockcake down to 1hp, then absorption potion to 1000 HP
+The absorb pots randomly wait between 1-4 'thresholds' that you've set and 
+will drink the proper amount of doses associated with the threshold multipler.
 
-    * Have your terminal open, flash the HP prayer to reset HP and run python nmz_bot.py
-
-    * Enjoy your (mostly) free XP ;) you'll probably need to click absoprtions every
-    once an hour or so but that's about it!
-
+Enjoy!
 """
-import pyautogui as auto, sys
+import pyautogui as auto
 import sys
 import random
 import time
-import threading
 
 # We assume that we are taking in 5 super combat pots, 
 # in the first row/left first spot of second
 POTS = [
-    {'coords':[582, 451], 'doses': 4},
-    {'coords': [624, 451], 'doses': 4},
-    {'coords': [666, 451], 'doses': 4},
-    {'coords': [708, 451], 'doses': 4},
-    {'coords': [584, 491], 'doses': 4}
+  {'coords': [582, 451], 'doses': 4},
+  {'coords': [624, 451], 'doses': 4},
+  {'coords': [666, 451], 'doses': 4},
+  {'coords': [708, 451], 'doses': 4},
+  {'coords': [582, 491], 'doses': 4}
+]
+
+ABSORBS = [
+  {'coords': [582, 451], 'doses': 4},
+  {'coords': [624, 451], 'doses': 4},
+  {'coords': [666, 451], 'doses': 4},
+  {'coords': [708, 451], 'doses': 4},
+  {'coords': [582, 451], 'doses': 4},
+  {'coords': [624, 451], 'doses': 4},
+  {'coords': [666, 451], 'doses': 4},
+  {'coords': [708, 451], 'doses': 4},
+  {'coords': [582, 451], 'doses': 4},
+  {'coords': [624, 451], 'doses': 4},
+  {'coords': [666, 451], 'doses': 4},
+  {'coords': [708, 451], 'doses': 4},
+  {'coords': [582, 451], 'doses': 4},
+  {'coords': [624, 451], 'doses': 4},
+  {'coords': [666, 451], 'doses': 4},
+  {'coords': [708, 451], 'doses': 4},
+  {'coords': [582, 451], 'doses': 4},
+  {'coords': [624, 451], 'doses': 4},
+  {'coords': [666, 451], 'doses': 4},
+  {'coords': [708, 451], 'doses': 4},
 ]
 
 # Use the prayer orb
@@ -80,80 +77,75 @@ HP_Y = [305, 315]
 # except KeyboardInterrupt:
 #     print('\n')
 
+def flash_prayorb():
+  # Weird behaviour with using the auto.click=(clicks=2) but below works.
+  auto.moveTo(random.randint(HP_X[0], HP_X[1]), random.randint(HP_Y[0], HP_Y[1]), 0.5)
+  auto.click()
+  time.sleep(random.uniform(0.3, 0.6))
+  auto.click()
 
-class NmzBot(object):
-  """
-  Main Bot Thread for handling drinking potion events.
+def drink_pots():
+  for pot in POTS:
+    # If still doses in this pot, drink. If not check next
+    if pot['doses'] > 0:
+      x, y = pot['coords'][0], pot['coords'][1]
+      auto.moveTo(random.randint(x-4, x+4), random.randint(y-4, y+5), 0.5)
+      auto.click()
+      pot['doses'] -= 1
+      break
 
-  Attributes:
-    timer (obj): Timer thread object
-    is_running (bool): Boolean for checking if timer thread is running.
-    next_call(int): Current time when started the thread
-  """
-
-  def __init__(self):
-    self._timer = None
-    self.is_running = False
-    # For init, we don't want to drink any absorb.
-    self.is_initial_call = True
-    self.next_call = time.time()
-    self.start()
-
-  def _run(self):
-    self.is_running = False
-    self.drink_super_combat()	
-    self.start()
-    
-  def start(self):
-    if not self.is_running:
-      # For range/mage use super pots
-      self.next_call += random.randint(850, 910)	
-      
-      # For melee: drink super combat at an interval of about ~17/18 mins
-      # self.next_call += random.randint(950, 970)
-      
-      self._timer = threading.Timer(self.next_call - time.time(), self._run)
-      self._timer.daemon = True # Program will end on keyboard input
-      self._timer.start()
-      self.is_running = True
-
-  def drink_super_combat(self):
-    for pot in POTS:
+def drink_absorbs(doses):
+  for _ in range(doses):
+    for pot in ABSORBS:
       # If still doses in this pot, drink. If not check next
       if pot['doses'] > 0:
         x, y = pot['coords'][0], pot['coords'][1]
         auto.moveTo(random.randint(x-4, x+4), random.randint(y-4, y+5), 0.5)
         auto.click()
         pot['doses'] -= 1
+        time.sleep(random.uniform(1.5, 2.2))
         break
-    
-    # Move back to prayer orb
-    auto.moveTo(random.randint(HP_X[0], HP_X[1]), random.randint(HP_Y[0], HP_Y[1]), 0.5)
-
-
-def flash_prayorb():
-  # Click the quick prays on and off. Weird behaviour with using the auto.click=(clicks=2)
-  # but below works.
-  auto.moveTo(random.randint(HP_X[0], HP_X[1]), random.randint(HP_Y[0], HP_Y[1]), 0.5)
-  auto.click()
-  time.sleep(random.uniform(0.3, 0.6))
-  auto.click()
-
 
 def main():
   print('Press Ctrl-C to quit.')
   
   # Initial reset of HP to start program
   flash_prayorb()
-  # The bot class handles drinking super combat pots (And absoprtions, if specified)
-  bot = NmzBot()
-  bot._run()
+  drink_pots()
+
+  # threshold time for repotting (seconds) & one dose of absorb used
+  repot_threshold, absorb_threshold = 600, 200
+  # random 'multiplier' for how long to wait before drinking absorbs
+  # (multiplier * threshold) and obviously the num doses (1 * multiplier)
+  absorb_threshold_multiplier = random.randint(1, 4)
+  
+  repot_start_time = time.time()
+  absorb_start_time = time.time()
+  drank_pots = False
 
   try:
       while True:
           # hp resets every min, so we reset every 45-50 seconds.
-          time.sleep(random.randint(45, 50))
+          # If we drank pots (mainly absorbs) that'll take some time... so reset faster
+          time.sleep(random.randint(35, 40)) if drank_pots else time.sleep(random.randint(45, 50)) 
           flash_prayorb()
+          drank_pots = False
+
+          # check to see if we need to repot.
+          if (time.time() - repot_start_time) > repot_threshold:
+            drink_pots()
+            repot_start_time, drank_pots = time.time(), True
+      
+          # check to see if we need to drink absorbs. 
+          if (time.time() - absorb_start_time) > (absorb_threshold * absorb_threshold_multiplier):
+            drink_absorbs(absorb_threshold_multiplier)
+            absorb_start_time, absorb_threshold_multiplier = time.time(), random.randint(1, 4)
+            drank_pots = True
+          
+          # if we drank a pot, move back to prayer orb
+          if drank_pots:
+            auto.moveTo(random.randint(HP_X[0], HP_X[1]), random.randint(HP_Y[0], HP_Y[1]), 0.5)
+
   except (KeyboardInterrupt, SystemExit):
       sys.exit(0)
 
